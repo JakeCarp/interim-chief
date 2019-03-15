244623098c627d65a43fbefd0e2af56551e9d334
JakeCarp <jakecarpenter407@gmail.com>
2019-03-15 15:15:33 -0600

    auth

---
+++ Server/server-assets/auth/session.js
@@ -0,0 +1,24 @@
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongodb-session")(expressSession);
+
+var store = new mongoStore({
+  uri: 'mongodb://student1:student1@ds062339.mlab.com:62339/kanban-checkpoint', //CHANGE ME!!!!!!
+  collection: "Sessions"
+});
+
+store.on("error", function (err) {
+  console.log("[SESSION ERROR]", err);
+});
+
+// @ts-ignore
+var session = expressSession({
+  secret: "The Corgi Howls at Sundown",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
+  },
+  store,
+  resave: true,
+  saveUninitialized: true
+});
+
+module.exports = session;
